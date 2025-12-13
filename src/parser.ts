e8a7b57c7acad2e5752511828afd14dc6c4d7dbf
Koefte <henkvanwersch@icloud.com>
2025-12-13 11:56:02 +0100

    add gitignore

--- src/parser.ts
+++ src/parser.ts
@@ -1,10 +1,42 @@
 import { Token , Tokenizer , TokenType} from './tokenizer.js';
 
+class ExpectedNodes {
+    private nodes: Node[];
+    constructor(nodes: Node[]) {
+        this.nodes = nodes;
+    }
+    toBe(expectedTypes: NodeType[]): GettableNodes {
+        if (this.nodes.length !== expectedTypes.length) {
+            throw new Error(`Expected ${expectedTypes.length} nodes but got ${this.nodes.length}`);
+        }
+        for (let i = 0; i < this.nodes.length; i++) {
+            if (this.nodes[i].type !== expectedTypes[i]) {
+                throw new Error(`Expected node type ${expectedTypes[i]} but got ${this.nodes[i].type}`);
+            }
+        }
+        return new GettableNodes(this.nodes);
+    }
+}
+
+class GettableNodes {
+    private nodes: Node[];
+    constructor(nodes: Node[]) {
+        this.nodes = nodes;
+    }
+    get(): Node[] {
+        return this.nodes;
+    }
+}
+
 export class Parser {
     private tokens: Token[]
     constructor(tokens: Token[]) {
         this.tokens = tokens;
     }
+    public static expect(tokens: Token[]): ExpectedNodes{
+        return new ExpectedNodes(new Parser(tokens).parse());
+    }
+
     parse(begin = 0 , end = this.tokens.length): Node[] {
         const nodes: Node[] = [];
         let position = begin;
@@ -20,15 +52,15 @@ export class Parser {
                 position = Tokenizer.find(this.tokens,'newline',position); 
             }
             else if(token.type == 'identifier' && token.value == 'sum'){
-                let sumNode: SumNode = {
-                    type: 'sum',
-                    start: 0,
-                    end: 0,
-                    variable: "",
-                    body: {type: 'paragraph'}
-                }
                 Tokenizer.expect(this.tokens[position + 1]).toBe(TokenType.Oparen)
-                Parser.expect(this.tokens.slice(position + 1,Tokenizer.find(this.tokens,'close_paren',position))).toBe([NodeType.Equation,NodeType.Expression,NodeType.Expression])
+                let sumNodes: Node[] = Parser.expect(this.tokens.slice(position + 1,Tokenizer.find(this.tokens,'close_paren',position))).toBe([NodeType.Equation,NodeType.Expression,NodeType.Expression]).get()
+                let sumNode = {
+                    type: 'sum',
+                    start: sumNodes[0] as EquationNode,
+                    end: sumNodes[1] as ExpressionNode,
+                    body: sumNodes[2] as ExpressionNode
+                }
+                
                 nodes.push(sumNode);
             }
             else if(token.type == 'identifier'){
@@ -76,6 +108,16 @@ interface Node {
     type: string;
 }
 
+interface ExpressionNode extends Node {
+    type: 'plus' | 'fraction' | 'identifier';
+    value: string;
+}
+
+interface IdentiferNode extends ExpressionNode {
+    type: 'identifier';
+    name: string;
+}
+
 interface ParagraphNode extends Node {
     type: 'paragraph';
     content: string;
@@ -90,19 +132,18 @@ interface EquationNode extends Node {
 
 interface SumNode extends Node {
     type: 'sum';
-    start: number;
-    end:number;
-    variable: string;
-    body: Node;
+    start: EquationNode;
+    end:ExpressionNode;
+    body: ExpressionNode;
 }
 
-interface PlusNode extends Node {
+interface PlusNode extends ExpressionNode {
     type: 'plus';
     left: Node;
     right: Node;
 }
 
-interface FractionNode extends Node {
+interface FractionNode extends ExpressionNode {
     type: 'fraction';
     numerator: Node;
     denominator: Node;
